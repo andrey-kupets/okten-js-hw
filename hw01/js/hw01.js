5b41e4c09e95ae941062441e5a0a0ca11815c9fb
andrey-kupets <andrey.kupets@i.ua>
2020-10-27 19:31:55 +0300

    add js hw01

--- hw01/js/hw01.js
+++ hw01/js/hw01.js
@@ -1,19 +1,19 @@
 // - присвоить каждому из следующих значений свою переменную: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false 
 // Вывести каждую при помощи console.log , alert, document.write
 
-let a = 'hello';
-let b = 'owu';
-let c = 'com';
-let d = 'ua';
-let num1 = 1;
-let num2 = 10;
-let num3 = -999;
-let num4 = 123;
-let num5 = 3.14;
-let num6 = 2.7;
-let num7 = 16;
-let bull1 = true;
-let bull2 = false;
+// let a = 'hello';
+// let b = 'owu';
+// let c = 'com';
+// let d = 'ua';
+// let num1 = 1;
+// let num2 = 10;
+// let num3 = -999;
+// let num4 = 123;
+// let num5 = 3.14;
+// let num6 = 2.7;
+// let num7 = 16;
+// let bull1 = true;
+// let bull2 = false;
 
 // console.log(a);
 // alert(a);
@@ -71,18 +71,18 @@ let bull2 = false;
 // - переопределить каждую переменную из задания 1 дав им произвольные значения
 // Вывести каждую при помощи console.log , alert, document.write
 
-a = 'js';
-b = 'rules';
-c = 'every';
-d = 'time';
-num1 = 111;
-num2 = 44.8;
-num3 = -888;
-num5 = -3.14;
-num6 = 5*8-3;
-num7 = 555**2;
-bull1 = false;
-bull2 = true;
+// a = 'js';
+// b = 'rules';
+// c = 'every';
+// d = 'time';
+// num1 = 111;
+// num2 = 44.8;
+// num3 = -888;
+// num5 = -3.14;
+// num6 = 5*8-3;
+// num7 = 555**2;
+// bull1 = false;
+// bull2 = true;
 
 // console.log(a);
 // alert(a);
@@ -194,67 +194,127 @@ bull2 = true;
 
 // - Взять переменные из задания 4 и сконкатенировать их в одной переменной person.
 
-let person = secondName + ' ' + firstName + ' ' + patronymic;
+// let person = secondName + ' ' + firstName + ' ' + patronymic;
 
-document.write(person);
-
+// document.write(person);
 
 // - Взять задние 4 и 5 и применить его к ФИО всех членов своей семьи.
 
-let secondNameMother = prompt('Enter yr secondName');
-let firstNameMother = prompt('Enter yr firstName');
-let patronymicMother = prompt('Enter yr patronymic');
-let secondNameSon = prompt('Enter yr secondName');
-let firstNameSon = prompt('Enter yr firstName');
-let patronymicSon = prompt('Enter yr patronymic');
+// let secondNameMother = prompt('Enter yr secondName');
+// let firstNameMother = prompt('Enter yr firstName');
+// let patronymicMother = prompt('Enter yr patronymic');
+// let secondNameSon = prompt('Enter yr secondName');
+// let firstNameSon = prompt('Enter yr firstName');
+// let patronymicSon = prompt('Enter yr patronymic');
 
-let personMother = secondNameMother + ' ' + firstNameMother + ' ' + patronymicMother;
-let personSon = secondNameSon + ' ' + firstNameSon + ' ' + patronymicSon;
+// let personMother = secondNameMother + ' ' + firstNameMother + ' ' + patronymicMother;
+// let personSon = secondNameSon + ' ' + firstNameSon + ' ' + patronymicSon;
 
-document.write(personMother);
-
-document.write(personSon);
+// document.write(personMother);
+// document.write(personSon);
 
 // - при помощи prompt() получить 3 числа. Привести их к числовому типу при помощи +. вывести их в консоль.
-// - при помощи prompt() получить 4 числа. Привести их к числовому типу при помощи parseInt. Сложить их между собой записав результат в переменную result и вывести в консоль браузера
-// - при помощи prompt()  получить 3 числа с плавающей точекой. при помощи parseFloat привести их к соответсвующему типу. Сложить их между собой записав результат в переменную result и вывести в консоль браузера
-// - при помощи prompt()  получить 3 числа с плавающей точекой. Округлить их при помощи Math.round Сложить их между собой записав результат в переменную  и вывести в консоль браузера
-// - при помощи prompt()  получить 2 целых числа. Привести их к целочисленному типу. Первое число - это число которое будут возводить в степень. Второе число - это число которое является степенью. При помощи Math.pow возвести первое число в степень второго числа.
-// - При помощи оператора опредеоения типа typeof определить типы следующих переменных и вывести их в консоль let a = 100; let b = '100'; let c = true; let d = undefined;
+
+// let q1 = +prompt('Enter num1');
+// let q2 = +prompt('Enter num2');
+// let q3 = +prompt('Enter num3');
+
+// // - при помощи prompt() получить 4 числа. Привести их к числовому типу при помощи parseInt. 
+// // Сложить их между собой записав результат в переменную result и вывести в консоль браузера
+
+// let result = q1 + q2 + q3;
+// console.log(result);
+// - при помощи prompt()  получить 3 числа с плавающей точекой. при помощи parseFloat привести их к соответсвующему типу. 
+// Сложить их между собой записав результат в переменную result и вывести в консоль браузера
+// let w1 = +prompt('Enter num1');
+// let w2 = +prompt('Enter num2');
+// let w3 = +prompt('Enter num3');
+
+// let e1 = parseFloat(w1);
+// let e2 = parseFloat(w2);
+// let e3 = parseFloat(w3);;
+
+// let result = e1 + e2 + e3;
+
+// console.log(result);
+// - при помощи prompt()  получить 3 числа с плавающей точекой. Округлить их при помощи Math.round
+// Сложить их между собой записав результат в переменную  и вывести в консоль браузера
+
+// let r1 = +prompt('Enter num1');
+// let r2 = +prompt('Enter num2');
+// let r3 = +prompt('Enter num3');
+
+// let e1 = Math.round(r1);
+// let e2 = Math.round(r2);
+// let e3 = Math.round(r3);;
+
+// let result = e1 + e2 + e3;
+
+// console.log(result);
+
+// - при помощи prompt()  получить 2 целых числа. Привести их к целочисленному типу.
+// Первое число - это число которое будут возводить в степень.
+// Второе число - это число которое является степенью.
+// При помощи Math.pow возвести первое число в степень второго числа.
+
+// let r1 = +prompt('Enter num1');
+// let r2 = +prompt('Enter num2');
+
+// let d1 = parseInt(r1);
+// let d2 = parseInt(r2);
+
+// let ff = Math.pow (d1, d2)
+
+// console.log(ff);
+
+// - При помощи оператора опредеоения типа typeof определить типы следующих переменных 
+// и вывести их в консоль let a = 100; let b = '100'; let c = true; let d = undefined;
+
+
+// let a = 100;
+// let b = '100';
+// let c = true;
+// let d = undefined;
+
+// console.log(typeof(a));
+// console.log(typeof(b));
+// console.log(typeof(c));
+// console.log(typeof(d));
+
 // - Поставьте соответствующий оператор в выражениях что бы получился соответсвующий результат.
 //  В одинаковых выражениях в задании не использовать два одинаковых оператора.
 // Каждое выражение вложить в свою переменную
-//     5 ? 6 -> true
-//     5 ? 6 -> false
-//     5 ? 6 -> false
-//     5 ? 6 -> false
-//     10 ? 10 -> true
-//     10 ? 10 -> true
-//     10 ? 10 -> false
-//     10 ? 10 -> false
-//     10 ? 10 -> false
-//     123 ? '123' -> false
-//     123 ? '123' -> true
+//     5 ? 6 -> true   >
+//     5 ? 6 -> false    <
+//     5 ? 6 -> false    <=
+//     5 ? 6 -> false    ==
+//     10 ? 10 -> true    ==
+//     10 ? 10 -> true      >= 
+//     10 ? 10 -> false    !=
+//     10 ? 10 -> false    <
+//     10 ? 10 -> false     !>
+//     123 ? '123' -> false  ===
+//     123 ? '123' -> true   ==
 
 // Дополнительно:
 // - Посмотрев на следюующие выражения, скажите, каков будет вывод в консоль
-// console.log(132 > 100 && 45 < 12 );
-// console.log(34 > 33 && 23 < 90 );
-// console.log(99 > 100 && 45 > 12 );
-// console.log(132 > 100 || 45 < 12 );
-// console.log(111 > 11 || 45 < 111 );
-// console.log((111 > 11 || 45 < 111) && (132 > 100 || 45 < 12) );
-// console.log((111 > 11 || 45 < 56) || (132 > 100 || 45 < 12) );
-// console.log((132 > 100 && 45 < 12 ) && (132 > 100 || 45 < 12 ) );
-// console.log((111 > 11 || 45 < 111) || (99 > 100 && 45 > 12 ));
-// console.log(!!'-1');
-// console.log(!!-1);
-// console.log(!!'0');
-// console.log(!!'null');
-// console.log(!!'undefined');
-// console.log(!!(3/'owu'));
-// console.log((111 > 11 || 45 < 111) ||  !!'0');
-// console.log((!!111 == !!11 || 45 < 111) && (99 > 100 && 45 > 12 ));
+// console.log(132 > 100 && 45 < 12 );    false
+// console.log(34 > 33 && 23 < 90 );     true
+// console.log(99 > 100 && 45 > 12 );      false
+// console.log(132 > 100 || 45 < 12 );   true
+// console.log(111 > 11 || 45 < 111 );     true
+// console.log((111 > 11 || 45 < 111) && (132 > 100 || 45 < 12) );  false
+// console.log((111 > 11 || 45 < 56) || (132 > 100 || 45 < 12) );   true
+// console.log((132 > 100 && 45 < 12 ) && (132 > 100 || 45 < 12 ) );   true
+// console.log((111 > 11 || 45 < 111) || (99 > 100 && 45 > 12 ));    false
+// console.log(!!'-1');   true
+// console.log(!!-1);   true
+// console.log(!!'0');   true
+// console.log(!!'null');   true
+// console.log(!!'undefined');   true
+// console.log(!!(3/'owu'));    false
+// console.log((111 > 11 || 45 < 111) ||  !!'0');  true 
+// console.log((!!111 == !!11 || 45 < 111) && (99 > 100 && 45 > 12 ));  false 
 
 
 // CLASS
