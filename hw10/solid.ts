52f02c011b1052a626601d8e3825f9b2cdddbac6
andrey-kupets <andrey.kupets@i.ua>
2022-09-08 21:42:57 +0300

    open-closed: right way of principle

--- hw10/solid.ts
+++ hw10/solid.ts
@@ -1,19 +1,34 @@
 // 2 open-closed principle
-class Weapon {
-    type: string;
+
+interface Attacker {
+    attack: () => void; // add for right way of principle
+}
+
+class Weapon implements Attacker {
     damage: number;
     range: number;
 
-    constructor(type: string, damage: number, range: number) {
-        this.type = type;
+    constructor(damage: number, range: number) {
         this.damage = damage;
         this.range = range;
     }
 
     attack() {
         // Violation of the principle
-        this.type === 'sword' && console.log(`Удар мечом, урон: ${this.damage}`);
-        this.type === 'crossbow' && console.log(`Удар с арбалета, урон: ${this.damage}`);
+        // this.type === 'sword' && console.log(`Удар мечом, урон: ${this.damage}`);
+        // this.type === 'crossbow' && console.log(`Удар с арбалета, урон: ${this.damage}`);
+    }
+}
+
+class Sword extends Weapon {
+    attack() {
+        console.log(`Удар мечом, урон: ${this.damage}`);
+    }
+}
+
+class Crossbow extends Weapon {
+    attack() {
+        console.log(`Удар с арбалета, урон: ${this.damage}`);
     }
 }
 
@@ -30,16 +45,16 @@ class Character {
         this.weapon = newWeapon;
     }
 
-    attack() {
+    attack() { // don't need
         this.weapon.attack();
     }
 }
 
-const sword = new Weapon('sword', 15, 2);
+const sword = new Sword( 15, 2);
 const character = new Character('warrior', sword);
 character.attack();
 
-const crossbow = new Weapon('crossbow', 10, 100);
+const crossbow = new Crossbow( 10, 100);
 character.changeWeapon(crossbow);
 character.attack();
 
